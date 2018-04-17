d37b581048b873f2d34de205bed17673e13cd1c9
Alex Dunford <alextdunford@gmail.com>
2018-04-16 21:44:37 -0400

    Restructured project, added looped thread

--- healthitforward/src/app/thread-container/thread-container.component.ts
+++ healthitforward/src/app/thread-container/thread-container.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'hif-thread-container',
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ThreadContainerComponent implements OnInit {
 
+  items: any[] = [
+    { title: 'Thread 1' },
+    { title: 'Thread 2' },
+    { title: 'Thread 3' }
+  ];
   constructor() { }
 
   ngOnInit() {
--- healthitforward/src/app/thread/thread.component.ts
+++ healthitforward/src/app/thread/thread.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'hif-thread',
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ThreadComponent implements OnInit {
 
+  @Input()
+  items: any[] = [];
   constructor() { }
 
   ngOnInit() {
