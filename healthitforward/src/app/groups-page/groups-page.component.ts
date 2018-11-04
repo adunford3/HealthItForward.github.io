bac2bf151d71654dff890be621f0e7932bf776d5
Alex Dunford <alextdunford@gmail.com>
2018-11-04 16:02:51 -0500

    Added very basic group listing finally

--- healthitforward/src/app/groups-page/groups-page.component.ts
+++ healthitforward/src/app/groups-page/groups-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {UserService} from '../core/user.service';
+import {GroupService} from "../core/group.service";
+import {GroupModel} from "../core/group.model";
 
 @Component({
   selector: 'hif-groups-page',
@@ -8,11 +10,23 @@ import {UserService} from '../core/user.service';
 })
 export class GroupsPageComponent implements OnInit {
 
+  groups;
+
   constructor(
     public userService: UserService,
+    public groupService: GroupService
   ) {}
 
   ngOnInit() {
+    const g = this.groupService.getGroups().then(function(groups) {
+      console.log('Group Stuff Here:');
+      console.log(groups);
+      console.log('groupID: ' + groups[0].groupID);
+      return groups;
+    });
+    this.groups = Promise.resolve(g);
+
+    console.log(this.groups);
   }
 
   createGroup(groupTitle: String) {
