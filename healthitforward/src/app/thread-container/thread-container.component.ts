3a5d2d05e7351304726c527915811ba4f89e2de2
Alex Dunford <alextdunford@gmail.com>
2018-11-18 15:18:27 -0500

    Got basic thread title showing functionality, need backend function to support

--- healthitforward/src/app/thread-container/thread-container.component.ts
+++ healthitforward/src/app/thread-container/thread-container.component.ts
@@ -3,6 +3,7 @@ import {ActivatedRoute, ParamMap, Router} from '@angular/router';
 import {GroupService} from '../core/group.service';
 import {switchMap} from 'rxjs/operators';
 import {UserService} from '../core/user.service';
+import {ThreadServices} from "../core/thread.service";
 
 @Component({
     selector: 'hif-thread-container',
@@ -16,41 +17,52 @@ export class ThreadContainerComponent implements OnInit {
     // group: GroupModel;
     paramGroupID;
     group;
+    threadIds;
+    threads;
+    currThread;
 
 
     constructor(private route: ActivatedRoute,
                 private router: Router,
                 private service: GroupService,
-                private userService: UserService) {
-        this.route.params.subscribe(params => console.log(params));
+                private userService: UserService,
+                private threadService: ThreadServices) {
+        this.route.params.subscribe(params => this.paramGroupID = params["id"]);
     }
 
     ngOnInit() {
-        const g = this.service.getGroup("-LNbI5b4ST0Ytwldnjky").then(function (group) {
-            // console.log('Vertical Navbar groups loaded');
+        const g = this.service.getGroup(this.paramGroupID).then(function (group) {
+            console.log(group);
             return group;
         });
         // this.group = Promise.resolve(g);
-        const f = this.route.paramMap.pipe(
-            switchMap((params: ParamMap) =>
-                Promise.resolve(this.service.getGroup(params.get('id')).then(function (group) {
-                    return group;
-                })))
-        );
-        this.group = f;
-        console.log(this.group);
-        const paramTest = this.route.paramMap.pipe(
-            switchMap((params: ParamMap) => {
-                const pId = params.get('id');
-                console.log('pID: ' + pId);
-                return pId;
-            })
-        );
-        this.paramGroupID = paramTest;
-        console.log('param id: ' + this.paramGroupID);
+        // const f = this.route.paramMap.pipe(
+        //     switchMap((params: ParamMap) =>
+        //         Promise.resolve(this.service.getGroup(params.get('id')).then(function (group) {
+        //             return group;
+        //         })))
+        // );
+        this.group = Promise.resolve(g);
+        this.threadIds = this.group.threads;
+        // this.threads = this.getThreads();
+        console.log("paramGroupID: " + this.paramGroupID);
         this.subscribed = false;
         document.getElementById('unsubscribe').style.backgroundColor = 'gray';
     }
+
+    // getThreads() {
+    //     let threads: any[];
+    //     new Promise(this.threadIds.forEach(thread => {
+    //             const t = this.threadService.getThread(thread).then(function(thread) {
+    //                 return thread;
+    //             });
+    //             let currThread = Promise.resolve(t);
+    //             threads.push(currThread);
+    //         });
+    //     )
+    //     console.log(threads);
+    //     return threads;
+    // }
 
     subscribeToGroup() {
         this.subscribed = true;
